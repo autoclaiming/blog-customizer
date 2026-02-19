48c223eb7e5e44d01f24a9b6b64f3df2015e2ea2
Alex Climanov <superauclaimer@gmail.com>
2026-02-19 03:12:05 +0300

    Обновлены стили и компоненты

--- src/ui/arrow-button/index.tsx
+++ src/ui/arrow-button/index.tsx
@@ -1,11 +1,1 @@
-import React, { useState } from 'react';
-import { ArrowButton } from './ArrowButton';
-
 export { ArrowButton } from './ArrowButton';
-
-export const ArrowButtonExample = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => setIsOpen(prev => !prev);
-
-    return <ArrowButton isOpen={isOpen} onClick={handleClick} />;
-};
